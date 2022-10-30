bbbf1aba325d98a77a71a4b54aaa9c7de53f1fce
Mário Ferreira <marioo.netoo3@gmail.com>
2022-10-30 10:24:56 -0300

    refatoramento
    código refatorado

--- script.js
+++ script.js
@@ -1,126 +1,59 @@
-// saber quantos caminhões saiu, (preciso de um loop)
-// saber quais os motoristas, em quais caminhões e em quais lugares.
-// receber toda informação e criar um texto com ela.
-// msg padrão : Boa Noite Claúdio, foram X caminhões!
+quantos = parseInt(prompt("Quantos caminhões?"))
+msg = document.getElementById("msg")
+res = document.getElementById("res")
 
-let caminhão1 = document.querySelector("input#caminhão1")
-let motorista1 = document.querySelector("input#motorista1")
-let local1 = document.querySelector("input#local1")
+for (let i = 0; i < quantos; i++) {
 
-let caminhão2 = document.querySelector("input#caminhão2")
-let motorista2 = document.querySelector("input#motorista2")
-let local2 = document.querySelector("input#local2")
+    div = document.createElement("div")
+    div.setAttribute('class', 'caminhoes')
+    msg.appendChild(div)
 
-let caminhão3 = document.querySelector("input#caminhão3")
-let motorista3 = document.querySelector("input#motorista3")
-let local3 = document.querySelector("input#local3")
+    //criando input caminhao
+    label = document.createElement("label")
+    label.innerText = "Caminhão"
+    div.appendChild(label)
+    input = document.createElement("input")
+    input.type = "text"
+    div.appendChild(input)
 
-let caminhão4 = document.querySelector("input#caminhão4")
-let motorista4 = document.querySelector("input#motorista4")
-let local4 = document.querySelector("input#local4")
+    //criando input caminhoneiro
+    label = document.createElement("label")
+    label.innerText = "Caminhoneiro"
+    div.appendChild(label)
+    input = document.createElement("input")
+    input.type = "text"
+    div.appendChild(input)
 
-let caminhão5 = document.querySelector("input#caminhão5")
-let motorista5 = document.querySelector("input#motorista5")
-let local5 = document.querySelector("input#local5")
+    //criando input viagem
+    label = document.createElement("label")
+    label.innerText = "Viagem"
+    div.appendChild(label)
+    input = document.createElement("input")
+    input.type = "text"
+    div.appendChild(input)
 
-let caminhão6 = document.querySelector("input#caminhão6")
-let motorista6 = document.querySelector("input#motorista6")
-let local6 = document.querySelector("input#local6")
-
-let res = document.querySelector("div#res")
-
-
-let viagem1 = {
-  Caminhão: "",
-  Motorista: "",
-  Local:"",
-}
-let viagem2 = {
-  Caminhão: "",
-  Motorista: "",
-  Local:"",
-}
-let viagem3 = {
-  Caminhão: "",
-  Motorista: "",
-  Local:"",
-}
-let viagem4 = {
-  Caminhão: "",
-  Motorista: "",
-  Local:"",
-}
-let viagem5 = {
-  Caminhão: "",
-  Motorista: "",
-  Local:"",
-}
-let viagem6 = {
-  Caminhão: "",
-  Motorista: "",
-  Local:"",
 }
 
-
-
-
-function calcular() {
-
-  let quantos = parseInt(prompt("Quantos caminhões?"))
-  
-  
-    viagem1.Caminhão = caminhão1.value;
-    viagem1.Local = local1.value;
-    viagem1.Motorista = motorista1.value;
-
-    viagem2.Caminhão = caminhão2.value;
-    viagem2.Local = local2.value;
-    viagem2.Motorista = motorista2.value;
-
-    viagem3.Caminhão = caminhão3.value;
-    viagem3.Local = local3.value;
-    viagem3.Motorista = motorista3.value;
-    
-    viagem4.Caminhão = caminhão4.value;
-    viagem4.Local = local4.value;
-    viagem4.Motorista = motorista4.value;
-    
-    viagem5.Caminhão = caminhão5.value;
-    viagem5.Local = local5.value;
-    viagem5.Motorista = motorista5.value;
-
-    viagem6.Caminhão = caminhão6.value;
-    viagem6.Local = local6.value;
-    viagem6.Motorista = motorista6.value;
-
-      dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado");
-      monName = new Array ("janeiro", "fevereiro", "março", "abril", "Maio", "junho", "junho" ,"agosto", "setembro", "outubro", "novembro", "dezembro");
-      now = new Date;
-
-
-    if (quantos > 1){
-
-    res.innerHTML = "Boa noite Cláudio."+ `<br>`+ `${quantos}` + " caminhões carregados na " + dayName[now.getDay() ] + "!" + `<br><br>
-
-    ${viagem1.Caminhão} - ${viagem1.Motorista} - ${viagem1.Local} <br>
-
-    ${viagem2.Caminhão} - ${viagem2.Motorista} - ${viagem2.Local} <br>
-
-    ${viagem3.Caminhão} - ${viagem3.Motorista} - ${viagem3.Local} <br>
-
-    ${viagem4.Caminhão} - ${viagem4.Motorista} - ${viagem4.Local} <br>
-
-    ${viagem5.Caminhão} - ${viagem5.Motorista} - ${viagem5.Local} <br>
-    
-    ${viagem6.Caminhão} - ${viagem6.Motorista} - ${viagem6.Local} <br>`
-
-    } else{
-      res.innerHTML = "Boa noite Cláudio."+ `<br>`+ `${quantos}` + " caminhão carregado na " + dayName[now.getDay() ] + "!" + `<br><br>
-
-      ${viagem1.Caminhão} - ${viagem1.Motorista} - ${viagem1.Local} <br></br> `
+function exibirResposta()
+{
+    p = document.createElement("p")
+    if(quantos == 1)
+    {
+        p.innerHTML = "Boa noite! \nFoi "+ quantos + " caminhão.<br>" 
     }
-
-  }
-  
-
+    else
+    {
+        p.innerHTML = "Boa noite! \nForam "+ quantos + " caminhões.<br>" 
+    }
+    
+    listaInputs = document.getElementsByClassName("caminhoes")
+    for(i = 0; i <= listaInputs.length - 1; i++)
+    {
+        caminhao = listaInputs[i]
+        
+        p.innerHTML += "<br>"+caminhao.getElementsByTagName("input")[0].value + " - " + caminhao.getElementsByTagName("input")[1].value + " - " + caminhao.getElementsByTagName("input")[2].value +"\n"
+        res.appendChild(p)
+        
+    }
+}
 
